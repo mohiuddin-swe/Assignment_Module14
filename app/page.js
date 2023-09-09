8a42f6b82df7b927d1a115bc5ef31af702419fe7
Mohiuddin <101384674+mohiuddin-swe@users.noreply.github.com>
2023-09-10 00:52:38 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,10 +4,9 @@ export default function Home() {
   return (
     <div className="bg-yellow-500 p-40 m-200">
       <h1 className="text-3xl">Assignment 14</h1>
-      <div className=" flex gap-6 text-red-500 justify-center text-3xl bg-yellow-500">
+      <div className=" flex gap-6 text-green-500 justify-center text-3xl bg-yellow-500">
         <Link href={"/register"}>register</Link>
         <Link href={"/login"}>login</Link>
-        <Link href={"/dashboard"}>dashboard</Link>
       </div>
     </div>
   );
